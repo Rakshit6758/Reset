5745b229854700745d22da5cb38c08343bd55edb
Rakshit Kapoor <500067642@stu.upes.ac.in>
2019-02-28 10:01:11 +0530

    File created

---
+++ File1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int x,y,z;
+ 
+ cout<<"Truth Table Of XY+Z:\n";
+ cout<<"X"<<"\t"<<"Y"<<"\t"<<"Z"<<"\t"<<"XY+Z";
+ for(x=0;x<=1;x++)
+ {
+ for(y=0;y<=1;y++)
+ {
+ for(z=0;z<=1;z++)
+ {
+  if(x*y+z==2)
+  cout<<"\n"<<x<<"\t"<<y<<"\t"<<z<<"\t1";
+  else
+   cout<<"\n"<<x<<"\t"<<y<<"\t"<<z<<"\t"<<x*y+z<<endl;
+   }
+  }
+ } 
+  
+return(0);
+}
